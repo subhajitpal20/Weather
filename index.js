cacd9a1aa736aa87a8a169396881c443cf0bf830
Subhajit <subhapal82051@gmail.com>
2021-02-08 16:34:13 +0530

    Test changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const request = require('request');
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-    console.log('body:', body);
-  }
-});
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('The chagnes')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
